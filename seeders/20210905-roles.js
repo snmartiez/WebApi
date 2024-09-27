6cdd8672aa4c49be0cd7f32d19e48c4bf86fcfc8
Silfred Martinez <silfred.martinez@gmail.com>
2024-09-27 08:04:25 -0500

    login2

--- seeders/20210905-roles.js
+++ seeders/20210905-roles.js
@@ -1,7 +1,7 @@
 module.exports = {
     up: async (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert('Roles', [
-        { roleName: 'Admin', createdAt: new Date(), updatedAt: new Date() },
+        { roleName: 'Administrador', createdAt: new Date(), updatedAt: new Date() },
         { roleName: 'Profesor', createdAt: new Date(), updatedAt: new Date() },
         { roleName: 'Estudiante', createdAt: new Date(), updatedAt: new Date() }
       ]);
